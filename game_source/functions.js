b085ffc496feecca980d87437d9729ac4694c57e
Elias <camposylia@gmail.com>
2019-03-09 20:18:15 +0200

    Added some comments

--- game_source/functions.js
+++ game_source/functions.js
@@ -1,39 +1,52 @@
 function elt(tagName, properties, ...children) {
-    let dom = document.createElement(tagName);
-    Object.assign(dom, properties);
-    for (let child of children) {
-        if (typeof child === 'string') {
-            dom.appendChild(document.createTextNode(child));
-        } else dom.appendChild(child);
-    }
-    return dom;
+  /*
+  Returns a new dom element by tag name, with given properties.
+  It contains children, and child also could be a string.
+  */
+  let dom = document.createElement(tagName);
+  Object.assign(dom, properties);
+  for (let child of children) {
+    if (typeof child === 'string') {
+      dom.appendChild(document.createTextNode(child));
+    } else dom.appendChild(child);
+  }
+  return dom;
 }
 
-function renderAudios(linksObj, type = 'wav', looped = false) {
-    let musicTracks = {},
-    musicContainer = elt('div', {className:"audio"});
-    for (let name in linksObj) {
-        musicTracks[name] = elt('audio', null, 
-            elt('source', {
-                type:`audio/${type}`, 
-                src:linksObj[name]
-            }));
-        if (looped) musicTracks[name].setAttribute('loop', true);
-        musicContainer.appendChild(musicTracks[name]);
-    }
-    document.body.appendChild(musicContainer);
-    return musicTracks;
+function renderAudios(linksObj, type = 'wav', looped = false, volume = 0.5) {
+  /*
+  Return object with values, contains html audio elements
+  and keys, call purposes of given audio elements
+  */
+  let musicTracks = {},
+  musicContainer = elt('div', {className:"audio"});
+  for (let name in linksObj) {
+    musicTracks[name] = elt('audio', null,
+      elt('source', {
+        type:`audio/${type}`,
+        src:linksObj[name]
+      }));
+    musicTracks[name].volume = volume;
+    if (looped) musicTracks[name].setAttribute('loop', true);
+    musicContainer.appendChild(musicTracks[name]);
+  }
+  document.body.appendChild(musicContainer);
+  return musicTracks;
 }
 
 function prettyTime(msecs, intervals) {
-    let str = "", num;
-    intervals.forEach(interv => {
-        num = Math.floor(msecs / interv);
-        str += num < 10 ? ":0"+num : ":"+num;
-        msecs = msecs % interv;
-    });
-    return str.substr(1);
+  /*
+  Return time string in format 'xx:xx:xx'
+  */
+  let str = "", num;
+  intervals.forEach(interv => {
+    num = Math.floor(msecs / interv);
+    str += num < 10 ? ":0"+num : ":"+num;
+    msecs = msecs % interv;
+  });
+  return str.substr(1);
 }
+
 function delay(time) {
-    return new Promise(reslv => setTimeout(reslv, time));
+  return new Promise(resolve => setTimeout(resolve, time));
 }
--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 const GAME_CONSTS = {
-    fieldWidth: 800, // px
-    fieldHeigth: 600, //  TODO: Set adoptable size (if possible!)
-    sessionBeginDelay: 3000 // msec
+
+  // size in pixels (px)
+  fieldWidth: 800,
+  fieldHeigth: 600,
+  // time in miliseconds (msec)
+  sessionBeginDelay: 2500
 }
 const MUSIC_LINKS = {
-    menu:"music/menu.mp3",
-    game:"music/game.mp3"
+  menu:"music/menu.wav",
+  game:"music/game.wav"
 }
 
+document.addEventListener('DOMContentLoaded', run);
 
-let myGame = new Game(); 
-runGame(myGame);
+async function run() {
+  let game = new Game();
+  let {fieldWidth, fieldHeigth, sessionBeginDelay} = GAME_CONSTS;
+  let music = renderAudios(MUSIC_LINKS, 'wav', true);
+  let newField = new Field(fieldWidth, fieldHeigth);
 
-async function runGame(game) {
-    let {fieldWidth, fieldHeigth, sessionBeginDelay} = GAME_CONSTS;
-    let music = renderAudios(MUSIC_LINKS, 'mp3', true);
-    let newField = new Field(fieldWidth, fieldHeigth);
+  try {await music["menu"].play();}
+  /* Some browsers does not allow play media
+  until user will not interact with page */
+  catch (error) {console.error(error.message)}
 
-    try {await music["menu"].play();} 
-    catch (error) {console.error(error.message)} // WARNING: Autoplay is not allowed until user will not interact with page
-    await game.beginGame(newField);
-    await music["menu"].pause();
+  await game.beginGame(newField);
+  await music["menu"].pause();
 
-    while (true) {
-        try {
-            music["game"].load();
-            await music["game"].play();
-            await delay(sessionBeginDelay); // It's 
-        } catch (err) {console.error(err.message)} 
-        let result = await game.runSession();
-        await music["game"].pause();
-        await game.sessionResults(result);
-    }
-    //-Note: table is not supposed to be here !!!
-    // TODO: make .table property of game, and handle it instead separate variable
-} 
+  while (true) {
+    try {
+      music["game"].load();
+      await music["game"].play();
+      await delay(sessionBeginDelay);
+    } catch (err) {console.error(err.message)}
+    let result = await game.runSession();
+    await music["game"].pause();
+    await game.sessionResults(result);
+  }
+}
